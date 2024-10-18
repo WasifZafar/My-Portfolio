9b143dc97ea4a0412bbe0282374a996ca9696081
WasifZafar <wasifzafar123@gmail.com>
2024-10-18 11:37:46 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -336,10 +336,10 @@ function cvButton(){
 }
 function emailButton(){
     document.getElementById("emailButton").onclick = function() {
-        const email ="wasifzafar123@gmail.com";
+        // const email ="wasifzafar123@gmail.com";
         const subject = encodeURIComponent("Connect to you!"); // Customize subject
         const body = encodeURIComponent("Hello, I am invited to you....."); // Customize message body
-        window.location.href ='mailto:${wasifzafar123@gmail.com}?subject=${subject}&body=${body}';
+        window.location.href ='mailto:wasifzafar123@gmail.com?subject=${subject}&body=${body}';
     };
 
 }
